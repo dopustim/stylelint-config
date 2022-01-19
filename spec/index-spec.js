3064684c1b498b1c78c25dca923743c5ee2fc10b
Vladimir <dopustimvladimir@gmail.com>
2022-01-20 00:23:11 +0600

    Update tests

--- spec/index-spec.js
+++ spec/index-spec.js
@@ -1,76 +1,61 @@
 const stylelint = require("stylelint")
 const config = require("../")
+const fs = require("fs")
 
-describe("With rule \"color-named\"", () => {
+for (const ruleName in config.rules) {
 
-    const validFiles = [ "./spec/fixtures/color-named/valid.css" ]
-    const invalidFiles = [ "./spec/fixtures/color-named/invalid.css" ]
+    if (!fs.existsSync(`./spec/fixtures/${ruleName}`) ||
+        !fs.lstatSync(`./spec/fixtures/${ruleName}`).isDirectory())
+        continue
 
-    let res
+    describe(`With rule "${ruleName}"`, () => {
 
-    describe("valid file", () => {
+        const validFiles = [ `./spec/fixtures/${ruleName}/valid.css` ]
+        const invalidFiles = [ `./spec/fixtures/${ruleName}/invalid.css` ]
 
-        beforeAll(async () => {
-            res = await stylelint.lint({ config: config, files: validFiles })
-            res = res.results[0]
+        let res
+
+        describe("valid file", () => {
+
+            beforeAll(async () => {
+                res = await stylelint.lint({ config: config, files: validFiles })
+                res = res.results[0]
+            })
+            it("has no errors", () => {
+                expect(res.errored).toBeFalsy()
+            })
+            it("has no warnings", () => {
+                expect(res.warnings.length).toBeFalsy()
+            })
         })
-        it("has no errors", () => {
-            expect(res.errored).toBeFalsy()
-        })
-        it("has no warnings", () => {
-            expect(res.warnings.length).toBeFalsy()
-        })
-    })
 
-    describe("invalid file", () => {
+        describe("invalid file", () => {
 
-        beforeAll(async () => {
-            res = await stylelint.lint({ config: config, files: invalidFiles })
-            res = res.results[0]
-        })
-        it("has errors", () => {
-            expect(res.errored).toBeTruthy()
-        })
-        it("has error for rule \"color-named\"", () => {
-            expect(res.warnings[0].rule).toBe("color-named")
-            expect(res.warnings[0].severity).toBe("error")
-        })
-    })
-})
-
-describe("With rule \"length-zero-no-unit\"", () => {
-
-    const validFiles = [ "./spec/fixtures/length-zero-no-unit/valid.css" ]
-    const invalidFiles = [ "./spec/fixtures/length-zero-no-unit/invalid.css" ]
-
-    let res
-
-    describe("valid file", () => {
-
-        beforeAll(async () => {
-            res = await stylelint.lint({ config: config, files: validFiles })
-            res = res.results[0]
-        })
-        it("has no errors", () => {
-            expect(res.errored).toBeFalsy()
-        })
-        it("has no warnings", () => {
-            expect(res.warnings.length).toBeFalsy()
-        })
-    })
-
-    describe("invalid file", () => {
-
-        beforeAll(async () => {
-            res = await stylelint.lint({ config: config, files: invalidFiles })
-            res = res.results[0]
-        })
-        it("has warnings", () => {
-            expect(res.warnings.length).toBeTruthy()
-        })
-        it("has warning for rule \"length-zero-no-unit\"", () => {
-            expect(res.warnings[0].rule).toBe("length-zero-no-unit")
-            expect(res.warnings[0].severity).toBe("warning")
+            beforeAll(async () => {
+                res = await stylelint.lint({ config: config, files: invalidFiles })
+                res = res.results[0]
+            })
+            if (config.rules[ruleName].length === 2 &&
+                "severity" in config.rules[ruleName][1] &&
+                config.rules[ruleName][1].severity === "error") {
+                // Errors
+                it("has errors", () => {
+                    expect(res.errored).toBeTruthy()
+                })
+                it(`has error for rule "${ruleName}"`, () => {
+                    expect(res.warnings[0].rule).toBe(ruleName)
+                    expect(res.warnings[0].severity).toBe("error")
+                })
+            } else {
+                // Warnings
+                it("has warnings", () => {
+                    expect(res.warnings.length).toBeTruthy()
+                })
+                it(`has warning for rule "${ruleName}"`, () => {
+                    expect(res.warnings[0].rule).toBe(ruleName)
+                    expect(res.warnings[0].severity).toBe("warning")
+                })
+            }
         })
     })
-})
+}
